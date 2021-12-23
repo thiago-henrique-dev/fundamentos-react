f74756ab9ed2718f57b84e1dd0c22e3f06b641a5
thiago-henrique-dev <thiagohenriquedev@gmail.com>
2021-12-23 08:58:44 -0300

    Exibindo String na tela

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import ReactDom from "react-dom";
+
+
+const el = document.getElementById('root')  // Pegar um elemento pelo ID
+ReactDom.render('Olá React!!!', el) // el - vai renderizar na nossa página o react
+
+/*ReactDOM.render(
+    'Ola React',
+        document.getElementById('root')
+)*/ // Outra forma de realizar
