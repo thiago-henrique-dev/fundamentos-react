0d8caf469aa7ece84da810d4c8bbeeff22007824
thiago-henrique-dev <thiagohenriquedev@gmail.com>
2021-12-27 15:33:00 -0300

    Componente com Estado

--- src/components/comunicação/IndiretaPai.jsx
+++ src/components/comunicação/IndiretaPai.jsx
@@ -1,18 +1,20 @@
-import React from 'react'
+import React,{ useState } from 'react'
 import IndiretaFilho from './IndiretaFilho'
+
 
 export default props => {
     
-    let nome = '?'
-    let idade = 0
-    let nerd = false
+    const [nome, setNome] = useState('?')
+    const [idade, setIdade] = useState(0)
+    const [nerd , setNerd] = useState(false)
+    
+  
 
-    function fornecerInformacões(nomeParam, idadeParam, nerdParam){
-        nome = nomeParam
-        idade = idadeParam
-        nerd =  nerdParam
 
-        console.log(nomeParam, idadeParam, nerdParam)
+    function fornecerInformacões(nome, idade, nerd){
+            setNome(nome)
+            setIdade(idade)
+            setNerd(nerd)
     }
 
 
--- src/components/condicional/ParOuImpar.jsx
+++ src/components/condicional/ParOuImpar.jsx
@@ -4,7 +4,8 @@ export default props => {
     const isPar = props.numero % 2 === 0
     return (
         <div>
-            { isPar ? <span>Par</span> : 
+            { isPar ?
+             <span>Par</span> : 
               <span>Impar</span> }
         </div>
     )
